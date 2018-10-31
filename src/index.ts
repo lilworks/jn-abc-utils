9bf6d96f1d7ab8ff48f8408b811507e2be37aacf
lil-works <lilian@lil-works.com>
2018-10-31 11:34:43 +0100

    1.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,122 +1,126 @@
 export const Params = {
-  K:"",
-  octaverCenter: 2
-
-}
+  K: '',
+  octaverCenter: 2,
+};
 
 export const Init = (params: any) => {
-  Params.K = params.K
+  Params.K = params.K;
   return true;
-}
+};
 // eg ["C","D","E","F#","G","A","B"]
-export const KeyDef = (keyDef) => {
+export const KeyDef = keyDef => {
   const o = [];
   keyDef.forEach(k => {
-     o.push({
-       enharmo:Enharmo(k),
-       toneName:RootName(k)
-     });
-   });
+    o.push({
+      enharmo: Enharmo(k),
+      toneName: RootName(k),
+    });
+  });
   return o;
-}
-export const Octaver = (octave:number) => {
-  if(octave-Params.octaverCenter === 0){ return "";}
+};
+export const Octaver = (octave: number) => {
+  if (octave - Params.octaverCenter === 0) {
+    return '';
+  }
   let replaceChar;
-  octave-Params.octaverCenter > 0  ?  replaceChar = "'" : replaceChar = ","
-  let s="";
-  for(let i=0; i<Math.abs(octave-Params.octaverCenter);i++){s=s+replaceChar}
-  return s
-}
+  octave - Params.octaverCenter > 0 ? (replaceChar = "'") : (replaceChar = ',');
+  let s = '';
+  for (let i = 0; i < Math.abs(octave - Params.octaverCenter); i++) {
+    s = s + replaceChar;
+  }
+  return s;
+};
 
-export const Enharmo = (toneName:string) => {
+export const Enharmo = (toneName: string) => {
   const countB = (toneName.match(/b/g) || []).length;
   const countS = (toneName.match(/#/g) || []).length;
   let replaceChar;
   let c;
-  if(countB>0){
-    c=countB;
-    replaceChar = "_" ;
-  }else if(countS>0){
-    c=countS;
-    replaceChar = "^" ;
-  }else{
-    return "";
+  if (countB > 0) {
+    c = countB;
+    replaceChar = '_';
+  } else if (countS > 0) {
+    c = countS;
+    replaceChar = '^';
+  } else {
+    return '';
   }
-  let o = "";
-  for(let i=0; i<c; i++){o=o+replaceChar}
+  let o = '';
+  for (let i = 0; i < c; i++) {
+    o = o + replaceChar;
+  }
   return o;
-}
+};
 
-export const RootName = (toneName:string) => {
+export const RootName = (toneName: string) => {
   toneName = toneName.replace(new RegExp('#', 'g'), '');
   return toneName.replace(new RegExp('b', 'g'), '');
-}
+};
 
-export const WskeyToneToAbc = (toneName:string,octave:number) => {
-  return {enharmo:Enharmo(toneName),toneName:RootName(toneName), octave:Octaver(octave)}
-}
+export const WskeyToneToAbc = (toneName: string, octave: number) => {
+  return { enharmo: Enharmo(toneName), toneName: RootName(toneName), octave: Octaver(octave) };
+};
 
-export const JnToAbc = (jn) =>{
+export const JnToAbc = jn => {
   const s = [];
   jn.forEach(t => {
-      s.push(WskeyToneToAbc(t.toneName,t.octave))
-   });
-  return s
-}
+    s.push(WskeyToneToAbc(t.toneName, t.octave));
+  });
+  return s;
+};
 
-
-export const AbcSequenceElementResult = (previous: any , current: any) => {
+export const AbcSequenceElementResult = (previous: any, current: any) => {
   // first time
-  if(!previous){
+  if (!previous) {
     return current;
   }
   // not the first time but same value so dont change anything
-  if(current.enharmo === previous.enharmo){
-      return {toneName:current.toneName,octave:current.octave,enharmo:""}
+  if (current.enharmo === previous.enharmo) {
+    return { toneName: current.toneName, octave: current.octave, enharmo: '' };
   }
 
   // Value are different so enharmo must change
-  if(current.enharmo !== previous.enharmo && previous.enharmo !== "="){
-      if(previous.enharmo === "" && current.enharmo !== ""){
-        return {toneName:current.toneName,octave:current.octave,enharmo:current.enharmo}
-      }
-      if(previous.enharmo !== ""  && current.enharmo === ""){
-        return {toneName:current.toneName,octave:current.octave,enharmo:"="}
-      }
-  }else if(previous.enharmo === "="){
-    if(current.enharmo === ""){
-      return {toneName:current.toneName,octave:current.octave,enharmo:""}
+  if (current.enharmo !== previous.enharmo && previous.enharmo !== '=') {
+    if (previous.enharmo === '' && current.enharmo !== '') {
+      return { toneName: current.toneName, octave: current.octave, enharmo: current.enharmo };
     }
-    if(current.enharmo === ""){
-      return {toneName:current.toneName,octave:current.octave,enharmo:current.enharmo}
+    if (previous.enharmo !== '' && current.enharmo === '') {
+      return { toneName: current.toneName, octave: current.octave, enharmo: '=' };
+    }
+  } else if (previous.enharmo === '=') {
+    if (current.enharmo === '') {
+      return { toneName: current.toneName, octave: current.octave, enharmo: '' };
+    }
+    if (current.enharmo === '') {
+      return { toneName: current.toneName, octave: current.octave, enharmo: current.enharmo };
     }
   }
-}
+};
 
 export const AbcSequence = (abcs: any) => {
-  const mem = []
-  const o = []
+  const mem = [];
+  const o = [];
   abcs.forEach(abc => {
-    let last ;
-    const r = mem.filter(x => x.toneName === abc.toneName)
-    if(r.length>0){
-      last = r[r.length-1]
+    let last;
+    const r = mem.filter(x => x.toneName === abc.toneName);
+    if (r.length > 0) {
+      last = r[r.length - 1];
     }
 
     o.push({
-      current:abc,
-      previous:last,
-      result:AbcSequenceElementResult(last,abc)
+      current: abc,
+      previous: last,
+      result: AbcSequenceElementResult(last, abc),
     });
-    mem.push(abc)
-   });
+    mem.push(abc);
+  });
   return o;
-}
+};
 
 export const AbcArrayToString = (abcArray: any) => {
-  let o = ""
+  let o = '';
   abcArray.forEach(abc => {
-    o = o + abc.result.enharmo + abc.result.toneName + abc.result.octave
-   });
+    o = o + abc.result.enharmo + abc.result.toneName + abc.result.octave;
+  });
   return o;
-}
+};
